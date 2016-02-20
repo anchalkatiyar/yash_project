85ced6455a680d16ed99594f43249c959c58b239
anchal <exit>
2016-02-21 00:12:03 +0530

    yash project 21 feb 2016

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YashProject::Application.config.secret_token = '5e40d961574b999d11b0c45873af03d441d6e30aefdabbccc27b9e837d241a0022e2e25aa862eaf4defb64d644223dbdcc0e92853ad888ff00a672e7975ee15c'
